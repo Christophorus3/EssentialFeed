40023c6cd0aa629d65c53db3b0e7545a8fcce330
Christoph Wottawa <christoph.wottawa@me.com>
2022-02-17 21:23:26 +0100

    ADD load test

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,11 +8,17 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
     var requestedURL: URL?
+    
+    private init() {}
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
@@ -26,15 +32,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     func test_init_noDataRequested() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
-        
-        
         
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_init() {
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
         
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
