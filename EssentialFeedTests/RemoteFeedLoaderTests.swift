5bdcaec6447f7404c46bb2dc823f0570fcc4a4f1
Christoph Wottawa <christoph.wottawa@netcetera.com>
2022-09-23 16:45:49 +0200

    ADD deliver empty item array from json

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -66,6 +66,23 @@ class RemoteFeedLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoItemsOnHTTP200AndEmptyJSONList() {
+        let (sut, client) = makeSUT()
+
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let listJSON = """
+        {
+            "items": []
+        }
+        """
+        let data = Data(listJSON.utf8)
+        client.complete(withStatusCode: 200, data: data)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!,
