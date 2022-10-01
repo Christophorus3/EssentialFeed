a8922e3ede2b35e0c60e803b5e297a63d9a3fcc7
Christoph Wottawa <christoph.wottawa@netcetera.com>
2022-10-01 12:01:53 +0200

    remove leftover comment

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-// needs to be equatable, but we dont know if the Error is equatable
-// therefore make it generic
 public enum LoadFeedResult {
     case success([FeedItem])
     case failure(Error)
