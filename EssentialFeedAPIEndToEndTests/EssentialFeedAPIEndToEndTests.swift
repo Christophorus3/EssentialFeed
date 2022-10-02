4137ce54e6639e02afc84deb414eb27845ddb04d
Christoph Wottawa <christoph.wottawa@netcetera.com>
2022-10-02 15:26:05 +0200

    extract test setup to helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -31,6 +31,21 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+        switch getFeedResult() {
+        case let .success(items):
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test feed")
+            
+            items.enumerated().forEach { (index,item) in
+                XCTAssertEqual(item, expectedItem(at: index))
+            }
+        case let .failure(error):
+            XCTFail("Expected successful feed result, got \(error) instead")
+        default:
+            XCTFail("Expected successful feed result, got no result instead")
+        }
+    }
+    
+    func getFeedResult() -> LoadFeedResult? {
         let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: url, client: client)
@@ -43,19 +58,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 10.0)
-        
-        switch receivedResult {
-        case let .success(items):
-            XCTAssertEqual(items.count, 8, "Expected 8 items in the test feed")
-            
-            items.enumerated().forEach { (index,item) in
-                XCTAssertEqual(item, expectedItem(at: index))
-            }
-        case let .failure(error):
-            XCTFail("Expected successful feed result, got \(error) instead")
-        default:
-            XCTFail("Expected successful feed result, got no result instead")
-        }
+        return receivedResult
     }
     
     private func expectedItem(at index: Int) -> FeedItem {
