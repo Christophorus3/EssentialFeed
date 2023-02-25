730e2d1b73760f1fdc90e068eeb7d9b5daea2c88
Christoph Wottawa <Christoph.Wottawa@netcetera.com>
2023-02-25 15:11:23 +0100

    retrieving from empty store delivers emtpy result

---
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -0,0 +1,37 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Christoph Wottawa on 25.02.23.
+//
+
+import XCTest
+import EssentialFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+final class CodableFeedStoreTests: XCTestCase {
+
+    func test_retrieve_devliversEmptyOnEmptyStore() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "wait for store retrieval")
+        
+        sut.retrieve() { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+
+}
