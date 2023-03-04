62ea6ccd265bc8471b2b3fd553365b0ae3c5bbda
Christoph Wottawa <Christoph.Wottawa@netcetera.com>
2023-03-04 10:30:36 +0100

    add empty `CoreDataFeedStoreTests`

---
+++ EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Christoph Wottawa on 04.03.23.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_devliversEmptyOnEmptyStore() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyStore() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyStore() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyStore() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedStoreValues() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyStore() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedStore() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
