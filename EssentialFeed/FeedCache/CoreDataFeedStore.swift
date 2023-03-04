d22f9589cad50c83ecdd564eaebb95cb291ed689
Christoph Wottawa <Christoph.Wottawa@netcetera.com>
2023-03-04 11:05:40 +0100

    add `ManagedStore` and `ManagedFeedImage` model representations

--- EssentialFeed/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -5,7 +5,7 @@
 //  Created by Christoph Wottawa on 04.03.23.
 //
 
-import Foundation
+import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
     public init() {}
@@ -20,5 +20,19 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         
+    }
+    
+    // MARK: - CoreData Models
+    private class ManagedStore: NSManagedObject {
+        @NSManaged var timestamp: Date
+        @NSManaged var feed: NSOrderedSet
+    }
+    
+    private class ManagedFeedImage: NSManagedObject {
+        @NSManaged var id: UUID
+        @NSManaged var imageDescription: String?
+        @NSManaged var location: String?
+        @NSManaged var url: URL
+        @NSManaged var store: ManagedStore
     }
 }
