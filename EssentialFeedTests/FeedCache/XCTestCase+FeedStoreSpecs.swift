27cf02c64bb6c1abc4aee7791197eed833194b8c
Christoph Wottawa <Christoph.Wottawa@netcetera.com>
2023-03-04 20:14:27 +0100

    increase waiting time to prevent test from failing every now and then

--- EssentialFeedTests/FeedCache/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeedTests/FeedCache/XCTestCase+FeedStoreSpecs.swift
@@ -126,7 +126,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 2.0)
+        wait(for: [exp], timeout: 5.0)
         return deletionError
     }
     
