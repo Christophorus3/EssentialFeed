cfd4c3c9265ec7569fca509969db2d45e3545685
Christoph Wottawa <Christoph.Wottawa@netcetera.com>
2023-05-01 11:37:03 +0200

    simplify `CoreDataFeedStore` code with the new Result APIs

--- EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -18,43 +18,32 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            do {
-                if let store = try ManagedStore.find(in: context) {
-                    completion(.success(CachedFeed(feed: store.localFeed, timestamp: store.timestamp)))
-                } else {
-                    completion(.success(nil))
+            completion(Result {
+                try ManagedStore.find(in: context).map {
+                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
                 }
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            do {
+            completion(Result {
                 let managedStore = try ManagedStore.newUniqueInstance(in: context)
                 managedStore.timestamp = timestamp
                 managedStore.feed = ManagedFeedImage.images(from: feed, in: context)
-                
                 try context.save()
-                completion(.success(Void()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
-            do {
+            completion(Result {
                 try ManagedStore.find(in: context)
                     .map(context.delete)
                     .map(context.save)
-                completion(.success(Void()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
