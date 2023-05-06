8b4a3264b6f7b1dacce872cda3f41e4d8ccd8ffb
Christoph Wottawa <Christoph.Wottawa@netcetera.com>
2023-05-06 10:27:39 +0200

    make EssentialFeed and EssentialFeedTests targets build on macOS and iOS

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -47,7 +47,7 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpec {
         let storeURL = testSpecificStoreURL
         let sut = makeSUT(storeURL: storeURL)
         
-        try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+        try? "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
         
         expect(sut, toRetrieve: .failure(anyNSError()))
     }
